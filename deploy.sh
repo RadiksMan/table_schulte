66cf001e49bdc20b07ef8116fe888141e60b26e8
RadiksMan <radion91@gmail.com>
2019-03-24 17:58:08 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+# cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:RadiksMan/table_schulte.git master:gh-pages
+
+cd -
